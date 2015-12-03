5066ce8eec42afae26db2707386ba90fb9b7468c
Ignat Smirnov <mail@ignat.co.uk>
2015-12-03 04:52:13 +0300

    Added animation on accommodations; Added «why we are» section

--- js/app.js
+++ js/app.js
@@ -3,3 +3,5 @@ $(document).foundation();
 // Slide backgrounds
 $("#slide-one").backstretch("images/villa2.jpg");
 $("#slide-two").backstretch("images/villa.jpg");
+$("#slide-three").backstretch("images/villa2.jpg");
+$("#slide-four").backstretch("images/villa.jpg");

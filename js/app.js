cd64ea28da537a169995008a4875f9868e1e9e5d
Ignat Smirnov <mail@ignat.co.uk>
2015-12-08 18:54:56 +0300

    Main page

--- js/app.js
+++ js/app.js
@@ -5,3 +5,5 @@ $("#slide-one").backstretch("images/villa2.jpg");
 $("#slide-two").backstretch("images/villa.jpg");
 $("#slide-three").backstretch("images/villa2.jpg");
 $("#slide-four").backstretch("images/villa.jpg");
+// Reviews BG
+$("#customer_reviews").backstretch("images/review_bg.png");

1f912ff248b68177fc3bc813f0e41da17c73713e
Ignat Smirnov <mail@ignat.co.uk>
2015-12-08 18:55:48 +0300

    Merge branch 'master' into gh-pages
    # Conflicts:
    #	config.codekit
    #	css/site.css
    #	index.haml
    #	index.html
    #	scss/.sass-cache/1b180df5f9ec18ca7de683a34bb8d409ca8f554c/site.scssc
    #	scss/site.scss

--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,1 +1,1 @@
-$(document).foundation(),$("#slide-one").backstretch("images/villa2.jpg"),$("#slide-two").backstretch("images/villa.jpg"),$("#slide-three").backstretch("images/villa2.jpg"),$("#slide-four").backstretch("images/villa.jpg");
+$(document).foundation(),$("#slide-one").backstretch("images/villa2.jpg"),$("#slide-two").backstretch("images/villa.jpg"),$("#slide-three").backstretch("images/villa2.jpg"),$("#slide-four").backstretch("images/villa.jpg"),$("#customer_reviews").backstretch("images/review_bg.png");
